08a11f6c30fac2ff52ee7c624209523552f6ec58
Vinay Somawat <svinay1@student.nitw.ac.in>
2019-03-22 21:43:38 +0530

    make it better

--- app/routes/dsa.js
+++ app/routes/dsa.js
@@ -1,7 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-	titleToken() {
-    return this.get('l10n').t('Stacks');
-  }
 });
--- tests/acceptance/list-posts-test.js
+++ tests/acceptance/list-posts-test.js
@@ -1,7 +1,6 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { 
-	click,
 	currentURL,
 	visit
 } from '@ember/test-helpers';
@@ -10,15 +9,7 @@ module('Acceptance | list posts', function(hooks) {
   setupApplicationTest(hooks);
 
   test('should show rentals as the home page', async function (assert) {
-  	await visit('/');
-  	assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  await visit('/');
+  assert.equal(currentURL(), '/home', 'should redirect automatically');
   });
-
-  test('should link to information about the company.', async function (assert) {
-  	await visit('/');
-  	await click(".menu-about");
-  	assert.equal(currentURL(), '/about', 'should navigate to about');
-  });
-
-
 });
